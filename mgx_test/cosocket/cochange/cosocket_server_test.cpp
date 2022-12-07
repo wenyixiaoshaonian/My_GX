c90e82b496298590b39fca0565fe86adbb3f1950
caoboxi <541190157@qq.com>
2022-12-07 13:22:54 +0800

    modify demo

--- mgx_test/cosocket/cochange/cosocket_server_test.cpp
+++ mgx_test/cosocket/cochange/cosocket_server_test.cpp
@@ -9,21 +9,26 @@
 void server(void *arg)
 {
     struct timeval tv = { 0 };
+    Mgx_coroutine_scheduler *sch = Mgx_coroutine_scheduler::get_instance();
+    Mgx_coroutine *co = sch->get_current_coroutine();
     for(;;) {
         gettimeofday(&tv, nullptr);
         printf(">>=== time gone : %d us\n",tv.tv_usec);
+        co->yield();
     }
-    
-
 }
 
 int main(int argc, char *argv[])
 {
     int i;
+    Mgx_coroutine *co[500];
     for(i = 0;i<500;i++)
-        new Mgx_coroutine(server, nullptr);  // delete by scheduler when coroutine function run finished
+        co[i] = new Mgx_coroutine(server, nullptr);  // delete by scheduler when coroutine function run finished
+
     for (;;) {
-        Mgx_coroutine_scheduler::get_instance()->schedule();
+        for(i = 0;i<500;i++) {
+            co[i]->resume();
+        }
     }
 
     return 0;
